43a39b1ad9a34fca2bcef22032335da6b92eea2e
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-08-17 10:35:04 +0200

    Add files via upload

---
+++ Zadaca11AdvSerializingAndDeserializing/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+namespace Zadaca11AdvSerializingAndDeserializing
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Zoran",
+                Surname = "Malinovic",
+                Age = 20,
+                Courses = new List<Course>
+                 {
+                     new Course() { Name = "C#",  Grade = 10},
+                     new Course() { Name = "SQL", Grade = 10},
+                     new Course() { Name = "C++", Grade = 10}
+                 }
+            };
+
+            string jsonFilePath = "OnePerson.json";
+            string json = JsonConvert.SerializeObject(person, Formatting.Indented);
+            File.WriteAllText(jsonFilePath, json);
+            Console.WriteLine($"Person info saved to {jsonFilePath}");
+
+            Console.WriteLine("\n**** Reading and writing all person info **** ");
+
+            string jsonFromFile = File.ReadAllText(jsonFilePath);
+
+            Person deserializedPerson = JsonConvert.DeserializeObject<Person>(jsonFromFile);
+
+            Console.WriteLine("\nDeserialized Person object: ");
+            Console.WriteLine($" Name: {deserializedPerson.Name}");
+            Console.WriteLine($" Surname: {deserializedPerson.Surname}");
+            Console.WriteLine($" Age: {deserializedPerson.Age}");
+            Console.WriteLine("Courses: ");
+
+            foreach(Course course in deserializedPerson.Courses)
+                Console.WriteLine($"  {course.Name}: {course.Grade}");
+        }
+    }
+}
